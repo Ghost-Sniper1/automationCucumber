d01017b1c651ff88e1ac315ee84c19e757d1ea8c
TestSolutions <jeanjuste.testsolutions@gmail.com>
2025-07-29 15:17:18 +0200

    JUnit Runners and TestNG Runners Annotations

--- src/test/java/automationCucumber/JUnitRunnerTest.java
+++ src/test/java/automationCucumber/JUnitRunnerTest.java
@@ -1,10 +1,22 @@
-/*package automationCucumber;
-
-import io.cucumber.junit.Cucumber;
-import io.cucumber.junit.CucumberOptions;
-import org.junit.runner.RunWith;
-
-@RunWith(Cucumber.class)
-@CucumberOptions()
-public class JUnitRunnerTest {
-}*/
+//package automationCucumber;
+//
+//import io.cucumber.junit.Cucumber;
+//import io.cucumber.junit.CucumberOptions;
+//import org.junit.AfterClass;
+//import org.junit.BeforeClass;
+//import org.junit.runner.RunWith;
+//
+//@RunWith(Cucumber.class)
+//@CucumberOptions()
+//public class JUnitRunnerTest {
+//
+//    @BeforeClass
+//    public static void beforeClass(){
+//        System.out.println("\nIN BEFORE CLASS\n");
+//    }
+//
+//    @AfterClass
+//    public static void afterClass(){
+//        System.out.println("\nIN AFTER CLASS\n");
+//    }
+//}
